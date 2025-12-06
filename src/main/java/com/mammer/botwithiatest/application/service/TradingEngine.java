369d711035fb89c58d7cf6ebd4ce9c7a929b657a
mammer bennasri <mammer.bennasri@gmail.com>
2025-12-06 19:51:45 +0100

    Fix spring tests configuration and cleanup

--- src/main/java/com/mammer/botwithiatest/application/service/TradingEngine.java
+++ src/main/java/com/mammer/botwithiatest/application/service/TradingEngine.java
@@ -1,9 +1,6 @@
 package com.mammer.botwithiatest.application.service;
 
-import com.mammer.botwithiatest.domaine.exception.DomainException;
 import com.mammer.botwithiatest.domaine.model.TradeSignal;
-import com.mammer.botwithiatest.infrastructure.config.AppMode;
-import com.mammer.botwithiatest.infrastructure.config.ApplicationStatusService;
 import com.mammer.botwithiatest.infrastructure.mt5.MT5BridgeClient;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +9,10 @@ public class TradingEngine {
 
     private final MT5BridgeClient bridgeClient;
     private final RiskManagementService riskManagementService;
-    private final ApplicationStatusService applicationStatusService;
 
-    public TradingEngine(MT5BridgeClient bridgeClient, RiskManagementService riskManagementService, ApplicationStatusService applicationStatusService) {
+    public TradingEngine(MT5BridgeClient bridgeClient, RiskManagementService riskManagementService) {
         this.bridgeClient = bridgeClient;
         this.riskManagementService = riskManagementService;
-        this.applicationStatusService = applicationStatusService;
     }
 
     public boolean executeTrade(TradeSignal signal, double equity, double stopLossPips, double stopLossPrice, double takeProfitPrice) {
@@ -32,11 +27,7 @@ public class TradingEngine {
             return false;
         }
 
-        if (shouldBlockOrders(signal)) {
-            throw new IllegalStateException("Demo period expired; live order placement is blocked.");
-        }
-
-        switch (signal) {
+        return switch (signal) {
             case BUY -> bridgeClient.sendOrder("BUY", lotSize, stopLossPrice, takeProfitPrice);
             case SELL -> bridgeClient.sendOrder("SELL", lotSize, stopLossPrice, takeProfitPrice);
             case NONE -> bridgeClient.sendOrder("CLOSE", 0, 0, 0);
@@ -53,11 +44,5 @@ public class TradingEngine {
         if (stopLossPips <= 0 || stopLossPrice <= 0 || takeProfitPrice <= 0) {
             throw new IllegalArgumentException("Risk parameters must be greater than zero");
         }
-    }
-
-    private boolean shouldBlockOrders(TradeSignal signal) {
-        return applicationStatusService.getCurrentMode() == AppMode.DEMO
-                && applicationStatusService.isDemoExpired()
-                && signal != TradeSignal.NONE;
     }
 }
